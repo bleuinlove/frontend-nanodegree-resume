1508cf1bf9798aa073bc618b8147637f97f10a72
kerri alison <kerrivarner@gmail.com>
2017-07-06 17:22:04 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,31 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedRole = HTMLheaderRole.replace("%data%", "Front End Web Developer");
+$("#header").append(formattedRole);
+
+var formattedName = HTMLheaderName.replace("%data%", "Kerri Alison");
+$("#header").prepend(formattedName);
+
+
+var bio = {
+	"name" : "Kerri Alison"	,
+	"role" : "Front End Web Developer",
+	"contacts" : {
+		"email" : "kerrivarner@gmail.com",
+		"mobile" : "323-555-6654"
+		"github" : "@kerrialison",
+		"location" : "New York City"
+	},
+	"welcome" : "Welcome to my Front End Web Design Portfolio!",
+	"skills": ["HTML", "CSS", "JS", "jQuery", "Digital Photography & Editing", "Adobe Creative Suite", "Sketch"],
+}
+	
+$("#main").append(bio.skills);
+
+var contact = HTMLcontactGeneric.replace("%contact%", "contact:");
+var email = HTMLcontactGeneric.replace("%data%", "kerrivarner@gmail.com");
+
+$("#topContacts").prepend(contact);
+
+//$("#header").prepend(bio.role);
+//$("#header").prepend(bio.contact.length);
+//$("#header").prepend(bio.welcome);
+//$("#header").prepend(bio.skills.length);
